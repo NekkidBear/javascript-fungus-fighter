04ca25e4f4203de258061788b861c51cd393fea5
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-01 18:05:55 -0500

    create fungusHP and attackPoints global variables; define functions for each attack button

--- script.js
+++ script.js
@@ -3,18 +3,45 @@
 // Example:
 // let fungusHP = 100;
 
-function onReady() {
-    console.log("Ready to go!")
+let fungusHP = 100;
+let attackPoints = 100;
+// function onReady() {
+//     console.log("Ready to go!")
     
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
+//     // Make sure you check the index.html file! 
+//     // There are lots of buttons and things ready for you to hook into here!
     
     
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+//     // 🧠 Remember
+//     // - Handle events that ->
+//     // - Updates state which is ->
+//     // - Rendered to the DOM
+// }
+
+
+// onReady()
+
+function arcaneScepter(event){
+    // AP cost = 12
+    // HP damage = 14
+    console.log ("Arcane Scepter!");
 }
 
+function entangle(event){
+    // AP cost = 23
+    // HP damage = 9
+    console.log("Entangle!");
+    
+}
 
-onReady()
+function dragonBlade(event){
+    // AP cost = 38
+    // HP damage = 47
+    console.log("Dragon Blade!")
+}
+function starFire(event){
+    // AP cost = 33
+    // HP damage = 25
+    console.log("Star Fire!")
+    
+}
