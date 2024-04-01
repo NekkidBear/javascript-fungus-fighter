21837fdde3bbeacc5d3eb8f6bdf77007105ab284
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-01 18:11:34 -0500

    confirmed attack buttons trigger on-click events

--- script.js
+++ script.js
@@ -7,41 +7,36 @@ let fungusHP = 100;
 let attackPoints = 100;
 // function onReady() {
 //     console.log("Ready to go!")
-    
-//     // Make sure you check the index.html file! 
-//     // There are lots of buttons and things ready for you to hook into here!
-    
-    
-//     // 🧠 Remember
-//     // - Handle events that ->
-//     // - Updates state which is ->
-//     // - Rendered to the DOM
-// }
 
+    // Make sure you check the index.html file!
+    // There are lots of buttons and things ready for you to hook into here!
+    // 🧠 Remember
+    // Handle events that -> 
+    // Updates state which is ->
+    // - Rendered to the DOM
+// }
 
 // onReady()
 
-function arcaneScepter(event){
-    // AP cost = 12
-    // HP damage = 14
-    console.log ("Arcane Scepter!");
+function arcaneScepter(event) {
+  // AP cost = 12
+  // HP damage = 14
+  console.log("Arcane Scepter!");
 }
 
-function entangle(event){
-    // AP cost = 23
-    // HP damage = 9
-    console.log("Entangle!");
-    
+function entangle(event) {
+  // AP cost = 23
+  // HP damage = 9
+  console.log("Entangle!");
 }
 
-function dragonBlade(event){
-    // AP cost = 38
-    // HP damage = 47
-    console.log("Dragon Blade!")
+function dragonBlade(event) {
+  // AP cost = 38
+  // HP damage = 47
+  console.log("Dragon Blade!");
 }
-function starFire(event){
-    // AP cost = 33
-    // HP damage = 25
-    console.log("Star Fire!")
-    
+function starFire(event) {
+  // AP cost = 33
+  // HP damage = 25
+  console.log("Star Fire!");
 }
